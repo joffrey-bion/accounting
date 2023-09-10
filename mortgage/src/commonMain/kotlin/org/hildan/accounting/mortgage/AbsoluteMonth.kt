833f54416b5dd860255ec6efb54ecb45bac46ccf
Joffrey Bion <joffrey.bion@gmail.com>
2023-09-10 23:35:38 +0200

    Add support for adding multiple months to AbsoluteMonth

--- mortgage/src/commonMain/kotlin/org/hildan/accounting/mortgage/AbsoluteMonth.kt
+++ mortgage/src/commonMain/kotlin/org/hildan/accounting/mortgage/AbsoluteMonth.kt
@@ -1,10 +1,21 @@
 package org.hildan.accounting.mortgage
 
+/**
+ * Represents a local date with month precision, so basically a month and a year.
+ */
 data class AbsoluteMonth(val year: Int, val month: Int) {
 
-    fun next() = AbsoluteMonth(
-        year = if (month == 12) year + 1 else year,
-        month = month % 12 + 1,
+    /**
+     * Returns the absolute month, coming after this one.
+     */
+    fun next() = plusMonths(1)
+
+    /**
+     * Returns the absolute month corresponding to [n] months after this one.
+     */
+    fun plusMonths(n: Int) = AbsoluteMonth(
+        year = year + (month - 1 + n) / 12,
+        month = (month - 1 + n) % 12 + 1,
     )
 
     override fun toString(): String = "$year-${month.toString().padStart(2, '0')}"
