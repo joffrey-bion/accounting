56676dbb865e23c3beacca1e327efc74f840a04c
Joffrey Bion <joffrey.bion@gmail.com>
2023-09-11 18:03:37 +0200

    Fix doc for payment

--- money/src/commonMain/kotlin/org/hildan/accounting/money/Payment.kt
+++ money/src/commonMain/kotlin/org/hildan/accounting/money/Payment.kt
@@ -1,6 +1,9 @@
 package org.hildan.accounting.money
 
 /**
- * A payment made at the end of a month.
+ * A dated payment.
+ *
+ * It can represent any money transfer at the beginning or end of a month (it's unspecified by this class, and has to
+ * be inferred from the context)
  */
 data class Payment(val date: AbsoluteMonth, val amount: Amount)
