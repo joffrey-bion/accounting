ebdabea2bdb19490b942ea2f93593a815deeaeaf
Joffrey Bion <joffrey.bion@gmail.com>
2023-09-21 18:13:43 +0200

    Fix tax deduction item for salary

--- nl-taxes/src/commonMain/kotlin/org/hildan/accounting/taxes/TaxSystem.kt
+++ nl-taxes/src/commonMain/kotlin/org/hildan/accounting/taxes/TaxSystem.kt
@@ -31,18 +31,32 @@ class TaxSystem(
                 totalAmount = profile.grossAnnualSalary,
                 type = TaxItemType.INCOME,
             ))
-            add(TaxItem(
-                name = "Tax deductions on salary",
-                description = "Amounts of money subtracted from the gross salary to get the taxable salary",
-                totalAmount = profile.taxDeductions.sumOf { it.amount },
-                details = null,
-                breakdown = profile.taxDeductions,
-                type = TaxItemType.TAX_DEDUCTION,
-            ))
+            if (profile.rule30p) {
+                add(TaxItem(
+                    name = "30% ruling deduction on salary",
+                    description = "Makes the taxable salary 30% smaller due to the 30% rule eligibility",
+                    totalAmount = profile.grossAnnualSalary * 30.pct,
+                    details = "30% of the gross salary of ${profile.grossAnnualSalary.format(scale = 0)}",
+                    breakdown = null,
+                    type = TaxItemType.TAX_DEDUCTION,
+                ))
+            }
+            if (profile.taxDeductions.isNotEmpty()) {
+                add(TaxItem(
+                    name = "Tax deductions on salary",
+                    description = "Amounts of money subtracted from the gross salary before applying taxes",
+                    totalAmount = profile.taxDeductions.sumOf { it.amount },
+                    details = null,
+                    breakdown = profile.taxDeductions,
+                    type = TaxItemType.TAX_DEDUCTION,
+                ))
+            }
             add(TaxItem(
                 name = "Taxable gross annual salary",
                 description = "The part of the gross salary to which taxes are applied (after tax deductions)",
                 totalAmount = grossAnnualTaxableSalary,
+                details = "No tax deductions nor 30% ruling, hence why it's the same amount as the gross annual salary"
+                    .takeIf { !profile.rule30p && profile.taxDeductions.isEmpty() },
                 type = TaxItemType.INCOME,
             ))
             add(salaryTaxItem)
@@ -89,7 +103,7 @@ class TaxSystem(
                         name = "30% ruling deduction on bonus",
                         description = "Makes the taxable bonus 30% smaller due to the 30% rule eligibility",
                         totalAmount = profile.grossAnnualBonus * 30.pct,
-                        details = "30% of the gross bonus of ${profile.grossAnnualBonus}",
+                        details = "30% of the gross bonus of ${profile.grossAnnualBonus.format(scale = 0)}",
                         breakdown = null,
                         type = TaxItemType.TAX_DEDUCTION,
                     )
