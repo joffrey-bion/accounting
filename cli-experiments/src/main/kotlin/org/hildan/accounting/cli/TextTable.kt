070d35ca7dd4c93dcce5d78bce1bb60da4539622
Joffrey Bion <joffrey.bion@gmail.com>
2023-09-21 17:37:44 +0200

    Add support for different alignments in the console table

--- cli-experiments/src/main/kotlin/org/hildan/accounting/cli/TextTable.kt
+++ cli-experiments/src/main/kotlin/org/hildan/accounting/cli/TextTable.kt
@@ -5,7 +5,19 @@ interface Table<T> {
 }
 
 interface TableBuilder<T> {
-    fun column(header: String, width: Int? = null, getValue: T.() -> Any?)
+    fun column(
+        header: String,
+        dataAlign: Align = Align.RIGHT,
+        headerAlign: Align = Align.LEFT,
+        width: Int? = null,
+        getValue: T.() -> Any?,
+    )
+}
+
+enum class Align {
+    LEFT,
+    CENTER,
+    RIGHT;
 }
 
 fun <T> table(colSeparator: String = "  ", configure: TableBuilder<T>.() -> Unit): Table<T> =
@@ -14,25 +26,35 @@ fun <T> table(colSeparator: String = "  ", configure: TableBuilder<T>.() -> Unit
 private class TableImpl<T>(val colSeparator: String) : TableBuilder<T>, Table<T> {
     private val columns = mutableListOf<Column<T>>()
 
-    override fun column(header: String, width: Int?, getValue: T.() -> Any?) {
-        columns.add(Column(header, width, getValue))
+    override fun column(header: String, dataAlign: Align, headerAlign: Align, width: Int?, getValue: T.() -> Any?) {
+        columns.add(Column(header, headerAlign, dataAlign, width, getValue))
     }
 
     override fun format(data: Iterable<T>): String {
         val colValues = data.map { element -> columns.map { it.getValue(element).toString() } }
         val colWidths = columns.mapIndexed { i, col -> col.width ?: maxOf(col.header.length, colValues.maxOf { it[i].length }) }
-        val headerLine = columns.mapIndexed { i, col -> col.header.padEnd(colWidths[i]) }.joinToString(colSeparator)
+        val headerLine = columns.mapIndexed { i, col -> col.paddedHeader(colWidths[i]) }.joinToString(colSeparator)
         return buildString {
             appendLine(headerLine)
             append(colValues.joinToString("\n") { row ->
-                row.mapIndexed { i, cell -> cell.padStart(colWidths[i]) }.joinToString(colSeparator)
+                row.mapIndexed { i, cell -> cell.pad(columns[i].dataAlign, colWidths[i]) }.joinToString(colSeparator)
             })
         }
     }
 }
 
+private fun String.pad(align: Align, width: Int) = when(align) {
+    Align.LEFT -> padEnd(width)
+    Align.RIGHT -> padStart(width)
+    Align.CENTER -> padStart(width - (width - length) / 2).padEnd(width)
+}
+
 private data class Column<T>(
     val header: String,
+    val headerAlign: Align,
+    val dataAlign: Align,
     val width: Int?,
     val getValue: (T) -> Any?,
-)
+) {
+    fun paddedHeader(width: Int) = header.pad(headerAlign, width)
+}
