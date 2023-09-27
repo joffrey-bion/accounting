ed47fa29f5461be669847db63b59b738424e995b
Joffrey Bion <joffrey.bion@gmail.com>
2023-09-27 17:11:37 +0200

    Make AbsoluteMonth comparable

--- money/src/commonMain/kotlin/org/hildan/accounting/money/AbsoluteMonth.kt
+++ money/src/commonMain/kotlin/org/hildan/accounting/money/AbsoluteMonth.kt
@@ -3,7 +3,7 @@ package org.hildan.accounting.money
 /**
  * Represents a local date with month precision, so basically a month and a year.
  */
-data class AbsoluteMonth(val year: Int, val month: Int) {
+data class AbsoluteMonth(val year: Int, val month: Int) : Comparable<AbsoluteMonth> {
 
     /**
      * Returns the absolute month coming after this one.
@@ -17,6 +17,8 @@ data class AbsoluteMonth(val year: Int, val month: Int) {
         year = year + (month - 1 + n) / 12,
         month = (month - 1 + n) % 12 + 1,
     )
+
+    override fun compareTo(other: AbsoluteMonth): Int = compareValuesBy(this, other) { it.toString() }
 
     override fun toString(): String = "$year-${month.toString().padStart(2, '0')}"
 }
