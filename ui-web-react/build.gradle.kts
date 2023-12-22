d0b63b82d534030cdc217ee45772092c9b7544fa
Joffrey Bion <joffrey.bion@gmail.com>
2023-12-22 23:35:10 +0100

    Upgrade Kotlin to 1.9.21

--- ui-web-react/build.gradle.kts
+++ ui-web-react/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
     }
 
     sourceSets {
-        val jsMain by getting {
+        jsMain {
             dependencies {
                 implementation(project(":mortgage"))
                 implementation(platform(libs.kotlin.wrappers.bom.get()))
