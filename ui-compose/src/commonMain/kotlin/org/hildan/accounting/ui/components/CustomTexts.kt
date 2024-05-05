d82d8ebe8a2b83affaf1368242dc1be224badacb
Joffrey Bion <joffrey.bion@gmail.com>
2024-05-05 21:47:08 +0200

    Add modifier and options support in custom text views

--- ui-compose/src/commonMain/kotlin/org/hildan/accounting/ui/components/CustomTexts.kt
+++ ui-compose/src/commonMain/kotlin/org/hildan/accounting/ui/components/CustomTexts.kt
@@ -2,21 +2,93 @@ package org.hildan.accounting.ui.components
 
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.text.font.*
+import androidx.compose.ui.text.style.*
+import androidx.compose.ui.unit.*
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.format
 import org.hildan.accounting.money.*
+import kotlin.text.Typography
 
 @Composable
-fun AmountText(amount: Amount, scale: Int? = null, currencySymbol: String = "€") {
+fun AmountText(
+    amount: Amount,
+    modifier: Modifier = Modifier,
+    scale: Int? = 2,
+    currencySymbol: String = "€",
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+) {
     val formattedAmount = remember(amount, scale, currencySymbol) {
         val n = if (scale == null) amount.format() else amount.format(scale)
         "$n $currencySymbol"
     }
-    Text(formattedAmount)
+    Text(
+        text = formattedAmount,
+        modifier = modifier,
+        color = color,
+        fontSize = fontSize,
+        fontStyle = fontStyle,
+        fontWeight = fontWeight,
+        fontFamily = fontFamily,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        textAlign = textAlign,
+        lineHeight = lineHeight,
+        overflow = overflow,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        minLines = minLines,
+    )
 }
 
 @Composable
-fun LocalDateText(date: LocalDate) {
+fun LocalDateText(
+    date: LocalDate,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+) {
     val formattedDate = remember(date) { date.format(LocalDate.Formats.ISO) }
-    Text(formattedDate)
+    Text(
+        text = formattedDate,
+        modifier = modifier,
+        color = color,
+        fontSize = fontSize,
+        fontStyle = fontStyle,
+        fontWeight = fontWeight,
+        fontFamily = fontFamily,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        textAlign = textAlign,
+        lineHeight = lineHeight,
+        overflow = overflow,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        minLines = minLines,
+    )
 }
