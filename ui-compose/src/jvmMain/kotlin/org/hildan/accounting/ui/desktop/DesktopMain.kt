e7e14bd754cdf6927e582abc54460cd4f7754d83
Joffrey Bion <joffrey.bion@gmail.com>
2024-05-05 21:47:27 +0200

    Use bigger default window size on desktop

--- ui-compose/src/jvmMain/kotlin/org/hildan/accounting/ui/desktop/DesktopMain.kt
+++ ui-compose/src/jvmMain/kotlin/org/hildan/accounting/ui/desktop/DesktopMain.kt
@@ -3,12 +3,15 @@ package org.hildan.accounting.ui.desktop
 import androidx.compose.material.icons.*
 import androidx.compose.material.icons.filled.*
 import androidx.compose.ui.graphics.vector.*
+import androidx.compose.ui.unit.*
 import androidx.compose.ui.window.*
 import org.hildan.accounting.ui.*
 
 fun main() = application {
+    val state = rememberWindowState(width = 1366.dp, height = 768.dp)
     Window(
         title = "Accounting",
+        state = state,
 //        undecorated = true, // to remove the OS title bar from the window
         icon = rememberVectorPainter(Icons.Default.AccountBalance),
         onCloseRequest = ::exitApplication,
