186515e2de801f2844d938d161ab71e93601e174
Joffrey Bion <joffrey.bion@gmail.com>
2024-09-23 02:27:53 +0200

    Extract MortgagePayment to a separate file

---
+++ mortgage/src/commonMain/kotlin/org/hildan/accounting/mortgage/MortgagePayment.kt
@@ -0,0 +1,36 @@
+package org.hildan.accounting.mortgage
+
+import kotlinx.datetime.*
+import org.hildan.accounting.money.*
+
+/**
+ * A payment towards the mortgage.
+ */
+data class MortgagePayment(
+    /**
+     * The date this payment was made.
+     */
+    val date: LocalDate,
+    /**
+     * The amount used to pay back the mortgage, which is subtracted from the current mortgage balance as a result.
+     */
+    val principalReduction: Amount,
+    /**
+     * The amount invested voluntarily to repay a part of the mortgage this month on top of the mandatory payment.
+     * Like [principalReduction], it is subtracted from the current mortgage balance (principal) as a result.
+     */
+    val extraPrincipalReduction: Amount,
+    /**
+     * The interest paid to the bank for borrowing the money.
+     */
+    val interest: Amount,
+    /**
+     * The balance of the mortgage before making this payment.
+     */
+    val balanceBefore: Amount,
+) {
+    /**
+     * The total amount paid to the bank.
+     */
+    val total: Amount = principalReduction + extraPrincipalReduction + interest
+}
--- mortgage/src/commonMain/kotlin/org/hildan/accounting/mortgage/Simulation.kt
+++ mortgage/src/commonMain/kotlin/org/hildan/accounting/mortgage/Simulation.kt
@@ -155,38 +155,6 @@ data class SimulationResult(
 }
 
 /**
- * A payment towards the mortgage.
- */
-data class MortgagePayment(
-    /**
-     * The date this payment was made.
-     */
-    val date: LocalDate,
-    /**
-     * The amount used to pay back the mortgage, which is subtracted from the current mortgage balance as a result.
-     */
-    val principalReduction: Amount,
-    /**
-     * The amount invested voluntarily to repay a part of the mortgage this month on top of the mandatory payment.
-     * Like [principalReduction], it is subtracted from the current mortgage balance (principal) as a result.
-     */
-    val extraPrincipalReduction: Amount,
-    /**
-     * The interest paid to the bank for borrowing the money.
-     */
-    val interest: Amount,
-    /**
-     * The balance of the mortgage before making this payment.
-     */
-    val balanceBefore: Amount,
-) {
-    /**
-     * The total amount paid to the bank.
-     */
-    val total: Amount = principalReduction + extraPrincipalReduction + interest
-}
-
-/**
  * A summary of what happened with respect to the mortgage during a year.
  */
 data class MortgageYearSummary(
