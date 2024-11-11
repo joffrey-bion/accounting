dd036a005aed49a9ecffbf9532e3a4acc0ddd9f3
Joffrey Bion <joffrey.bion@gmail.com>
2024-11-11 02:25:45 +0100

    Update test data with real values

--- test-data/src/commonMain/kotlin/org/hildan/accounting/testdata/JHHisgenpadMortgage.kt
+++ test-data/src/commonMain/kotlin/org/hildan/accounting/testdata/JHHisgenpadMortgage.kt
@@ -13,7 +13,7 @@ private val optionsPrice = 56_913.eur
 // Based on initial options price estimate, and now recorded as such by Obvion
 private val estimatedWozValue = landPrice + constructionPrice + parkingPrice + 38_633.eur
 
-private val startDate = LocalDate(year = 2024, monthNumber = 11, dayOfMonth = 17)
+private val startDate = LocalDate(year = 2023, monthNumber = 11, dayOfMonth = 17)
 
 // From: https://www.obvion.nl/Hypotheek-rente/Actuele-hypotheekrente-Obvion?duurzaamheidskorting=Ja
 // The following rates were locked in on 2023-09-11
@@ -27,16 +27,17 @@ private val ObvionRateSept2023 = InterestRate.DynamicLtv(
     )
 )
 
+// these dates are not the dates of the bills, but the dates of the payments themselves
 private val constructionBillsPayments = listOf(
-    Payment(date = LocalDate.parse("2024-01-16"), amount = constructionPrice * 3.pct),
-    Payment(date = LocalDate.parse("2024-04-18"), amount = constructionPrice * 10.pct),
-    Payment(date = LocalDate.parse("2024-06-11"), amount = constructionPrice * 15.pct),
-    Payment(date = LocalDate.parse("2024-06-16"), amount = constructionPrice * 10.pct),
-    Payment(date = LocalDate.parse("2024-08-01"), amount = constructionPrice * 5.pct),      // TODO add real date
-    Payment(date = LocalDate.parse("2024-09-01"), amount = constructionPrice * "23.5".pct), // TODO add real date
-    Payment(date = LocalDate.parse("2024-10-01"), amount = constructionPrice * 10.pct),     // TODO add real date
-    Payment(date = LocalDate.parse("2024-11-01"), amount = constructionPrice * "13.5".pct), // TODO add real date
-    Payment(date = LocalDate.parse("2025-07-01"), amount = constructionPrice * 10.pct), // on delivery date
+    Payment(date = LocalDate.parse("2024-01-16"), amount = constructionPrice * 3.pct),      // T1
+    Payment(date = LocalDate.parse("2024-04-18"), amount = constructionPrice * 10.pct),     // T2
+    Payment(date = LocalDate.parse("2024-06-11"), amount = constructionPrice * 15.pct),     // T3
+    Payment(date = LocalDate.parse("2024-06-16"), amount = constructionPrice * 10.pct),     // T4
+    Payment(date = LocalDate.parse("2024-12-01"), amount = constructionPrice * 5.pct),      // T5 TODO add real date
+    Payment(date = LocalDate.parse("2025-01-15"), amount = constructionPrice * "23.5".pct), // T6 TODO add real date
+    Payment(date = LocalDate.parse("2024-11-11"), amount = constructionPrice * 10.pct),     // T7
+    Payment(date = LocalDate.parse("2024-11-01"), amount = constructionPrice * "13.5".pct), // T8 TODO add real date
+    Payment(date = LocalDate.parse("2025-09-01"), amount = constructionPrice * 10.pct), // on delivery date TODO add real date
 )
 
 val jhHisgenpadSimulationIncremental = SimulationSettings(
@@ -46,6 +47,10 @@ val jhHisgenpadSimulationIncremental = SimulationSettings(
         annualInterestRate = ObvionRateSept2023,
         startDate = startDate,
         termInYears = 30,
+        extraPayments = listOf(
+            Payment(LocalDate.parse("2024-06-07"), 50_000.eur),
+            Payment(LocalDate.parse("2024-06-13"), "12202.92".eur),
+        )
     ),
     property = Property.NewConstruction(
         initialNotaryPayment = Payment(startDate, landPrice),
