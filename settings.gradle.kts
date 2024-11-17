fa75932b4401a57d6cbae87ac2bd4b174eb13107
Joffrey Bion <joffrey.bion@gmail.com>
2024-11-18 00:27:57 +0100

    Add develocity for build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.gradle.develocity") version "3.18.1"
+}
+
 rootProject.name = "accounting"
 
 include("cli-experiments")
@@ -21,3 +25,11 @@ include("nl-taxes")
 include("test-data")
 include("ui-compose")
 include("ui-web-react")
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = false // background upload is bad for CI, and not critical for local runs
+    }
+}
