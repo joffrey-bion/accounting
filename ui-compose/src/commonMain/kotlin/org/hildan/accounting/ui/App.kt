403f6d064edc9894549cd7adfa67040ac1b3440c
Joffrey Bion <joffrey.bion@gmail.com>
2024-12-31 02:02:02 +0100

    Respect system dark theme

--- ui-compose/src/commonMain/kotlin/org/hildan/accounting/ui/App.kt
+++ ui-compose/src/commonMain/kotlin/org/hildan/accounting/ui/App.kt
@@ -1,10 +1,12 @@
 package org.hildan.accounting.ui
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.*
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import org.hildan.accounting.ui.screens.*
 
 enum class Screen {
@@ -15,12 +17,19 @@ enum class Screen {
 fun App() {
     var currentScreen by remember { mutableStateOf(Screen.MortgageSimulation) }
 
-    MaterialTheme {
-        Column {
-            Header(currentScreen = currentScreen, onNavigate = { currentScreen = it })
-            when (currentScreen) {
-                Screen.MortgageSimulation -> MortgageSimulationScreen()
-                Screen.IncomeTax -> IncomeTaxScreen()
+    val colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
+    MaterialTheme(colorScheme = colorScheme) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.onBackground,
+        ) {
+            Column {
+                Header(currentScreen = currentScreen, onNavigate = { currentScreen = it })
+                when (currentScreen) {
+                    Screen.MortgageSimulation -> MortgageSimulationScreen()
+                    Screen.IncomeTax -> IncomeTaxScreen()
+                }
             }
         }
     }
@@ -29,14 +38,24 @@ fun App() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun Header(currentScreen: Screen, onNavigate: (Screen) -> Unit) {
-    TopAppBar(title = { Text("Accounting") }, actions = {
-        IconButton(onClick = { onNavigate(Screen.MortgageSimulation) }) {
-            Icon(Icons.Default.CurrencyExchange, "Mortgage icon")
-        }
-        IconButton(onClick = { onNavigate(Screen.IncomeTax) }) {
-            Icon(Icons.Default.AccountBalance, "Taxes icon")
-        }
-    })
+    TopAppBar(
+        title = { Text("Accounting") },
+        colors = TopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            scrolledContainerColor = MaterialTheme.colorScheme.primaryContainer,
+        ),
+        actions = {
+            IconButton(onClick = { onNavigate(Screen.MortgageSimulation) }) {
+                Icon(Icons.Default.CurrencyExchange, "Mortgage icon")
+            }
+            IconButton(onClick = { onNavigate(Screen.IncomeTax) }) {
+                Icon(Icons.Default.AccountBalance, "Taxes icon")
+            }
+        },
+    )
 //    NavigationBar {
 //        NavigationBarItem(
 //            selected = currentScreen == Screen.Mortgage,
