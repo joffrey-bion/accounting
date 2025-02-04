b76857c3da44b9c74bee305b2ff9597ce31b75a5
Joffrey Bion <joffrey.bion@gmail.com>
2025-02-04 03:47:00 +0100

    Remove commit/cancel steps in payment list edition

--- ui-compose/src/commonMain/kotlin/org/hildan/accounting/ui/components/PaymentList.kt
+++ ui-compose/src/commonMain/kotlin/org/hildan/accounting/ui/components/PaymentList.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.unit.*
 import kotlinx.datetime.*
 import org.hildan.accounting.money.*
 import org.hildan.accounting.mortgage.*
-import org.hildan.accounting.ui.components.datepicker.*
 import org.hildan.accounting.ui.components.textinput.*
 
 @Composable
@@ -28,6 +27,9 @@ fun EditablePaymentList(
                 onValueChange = {
                     onValueChange(payments.toMutableList().apply { set(index, it) })
                 },
+                onDelete = {
+                    onValueChange(payments - installment)
+                }
             )
         }
     }
@@ -44,62 +46,36 @@ private fun createPaymentTodayZero() = Payment(
 )
 
 @Composable
-private fun EditablePaymentListItem(value: Payment, onValueChange: (Payment) -> Unit) {
-    var isEditing by remember { mutableStateOf(false) }
+private fun EditablePaymentListItem(value: Payment, onValueChange: (Payment) -> Unit, onDelete: () -> Unit) {
     ListItem(
         modifier = Modifier.width(IntrinsicSize.Min),
         headlineContent = {
             Row(verticalAlignment = Alignment.CenterVertically) {
-                if (isEditing) {
-                    PaymentListItemFields(
-                        initialValue = value,
-                        onEditCommit = { isEditing = false; onValueChange(it) },
-                        onEditCancel = { isEditing = false },
-                    )
-                } else {
-                    LocalDateText(value.date)
-                    Spacer(Modifier.width(5.dp))
-                    AmountText(value.amount)
-                    Spacer(Modifier.width(5.dp))
-                    Text(value.description)
-                    Spacer(Modifier.width(3.dp))
-                    IconButton(onClick = { isEditing = true }) {
-                        Icon(Icons.Default.Edit, contentDescription = "Edit")
-                    }
+                val onValueChange1 = { it: Payment -> onValueChange(it) }
+                LocalDateTextField(
+                    value = value.date,
+                    onValueChange = { onValueChange1(value.copy(date = it)) },
+                    modifier = Modifier.widthIn(max = 160.dp),
+                )
+                Spacer(Modifier.width(5.dp))
+                AmountTextField(
+                    value = value.amount,
+                    onValueChange = { onValueChange1(value.copy(amount = it)) },
+                    modifier = Modifier.width(150.dp),
+                )
+                Spacer(Modifier.width(5.dp))
+                OutlinedTextField(
+                    value = value.description,
+                    onValueChange = { onValueChange1(value.copy(description = it)) },
+                    modifier = Modifier.width(400.dp),
+                )
+                Spacer(Modifier.width(5.dp))
+                IconButton(
+                    onClick = onDelete,
+                ) {
+                    Icon(Icons.Default.Delete, "Remove payment")
                 }
             }
         },
     )
-}
-
-@Composable
-private fun PaymentListItemFields(
-    initialValue: Payment,
-    onEditCommit: (Payment) -> Unit,
-    onEditCancel: () -> Unit,
-) {
-    var date by remember(initialValue) { mutableStateOf(initialValue.date) }
-    var amount by remember(initialValue) { mutableStateOf(initialValue.amount) }
-    var description by remember(initialValue) { mutableStateOf(initialValue.description) }
-    DatePickerButton(
-        value = date,
-        onValueChange = { date = it ?: error("selected date should not be null") },
-        modifier = Modifier.width(150.dp),
-    )
-    AmountTextField(
-        value = amount,
-        onValueChange = { amount = it },
-        modifier = Modifier.width(150.dp),
-    )
-    OutlinedTextField(
-        value = description,
-        onValueChange = { description = it },
-        modifier = Modifier.width(400.dp),
-    )
-    IconButton(onClick = { onEditCommit(Payment(date = date, amount = amount, description = description)) }) {
-        Icon(Icons.Default.Check, contentDescription = "Confirm")
-    }
-    IconButton(onClick = onEditCancel) {
-        Icon(Icons.Default.Close, contentDescription = "Cancel")
-    }
 }
